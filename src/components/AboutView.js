e0e19225da7e110bf38b53204a221227f77dba8c
Brandon Johnson <brandonajohnson09@gmail.com>
2021-04-03 17:35:00 -0400

    Updated about view to include link to the api

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -8,15 +8,10 @@ const AboutView = () => {
         <div className="row">
           <div className="col-lg-8 offset-lg-2 my-5">
             <p className="lead">
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
-              this is some sample text. Additional sample text. this is some sample text. Additional sample text
+              This project originated from a Udemy course I was taking. It had the basic functionality of pulling from the API and displaying movies based on search.
+              I thought that the project was fun, and I decided to continue working on it. I have recently added a similar films view, and I intend to continue to expand
+              on the project in my free time. The documentation on the <a href='https://developers.themoviedb.org/3/getting-started/introduction' target='_blank' rel="noreferrer">TMDB API</a> is
+              pretty large and there are several available datasets that could be entertaining to add. Please feel free to expand on this idea in any way you would like.
             </p>
           </div>
         </div>
